b34aa7fa98e2f60d0158ab2222aee35365148a0c
yosuapurnomo <yosuapurnomo321@gmail.com>
2021-04-19 23:51:58 +0700

    push

--- biodata/settings.py
+++ biodata/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
