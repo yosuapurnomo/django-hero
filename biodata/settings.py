8a9314635a67bf526fb913483d2fda36e27b6ecf
yosuapurnomo <yosuapurnomo321@gmail.com>
2021-04-19 23:54:58 +0700

    push

--- biodata/settings.py
+++ biodata/settings.py
@@ -139,9 +139,9 @@ USE_TZ = True
 
 STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/staticfiles/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'staticfiles'),
+# )
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CORS_ALLOW_ALL_ORIGINS = True
